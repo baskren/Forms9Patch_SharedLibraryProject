5d167be0323a4eee17b2a1a2e6cc1532830dfb14
Ben Askren <ben@buildcalc.com>
2017-02-22 10:45:02 -0500

    Modified XAML layouts to work with Shared Assemblies Projects

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace SharedApp.Droid
 {
-	[Activity(Label = "SharedApp.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "SharedApp", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
